9585351b64ee6beef2b13ccc4b0a954614b9ea99
NoneTheWisr <12655815+NoneTheWisr@users.noreply.github.com>
2022-06-08 22:17:56 +0400

    finish "VGA Text Mode"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World!");
+    panic!("A panic message")
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{info}");
+    loop {}
 }
